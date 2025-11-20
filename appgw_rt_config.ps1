9f546b7dea434935fa54abd1ea1b6a212cc52636
YiYu-dev <yuta.contact03@gmail.com>
2025-11-20 18:33:39 +0900

    Update appgw_rt_config.ps1

--- appgw_rt_config.ps1
+++ appgw_rt_config.ps1
@@ -1,7 +1,7 @@
 $location = "japan east"
 $SubscriptionId = (az account show | convertfrom-json).id
 $tenantId = (az account show | convertfrom-json).tenantId
-$Id = "001"
+$Id = "002"
 $agwId = "01"
 $ResourceDefaultNm = "jp.rbs.wak-any"
 $ResourceDefaultNmhyphen = "jp-rbs-wak-any"
